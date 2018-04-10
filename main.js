76f3857b1961b06243d1e5023895f4d8e0c1685b
Justin Hays <jhays124@yahoo.com>
2018-04-10 09:16:41 -0700

    wholefoods populates db via axios call

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 const axios = require('axios')
-const scraper = require('./scrapers/target')
+const target = require('./scrapers/target')
+const wholefoods = require('./scrapers/wholefoods')
+
 //notdeployed
 const baseURL = 'http://localhost:3000/'
 
 //deployed
 //const baseURL ''
 
-scraper.targetScrape()
-  .then(data => scraper.stringsParser(data))
+// target.targetScrape()
+//   .then(data => target.stringsParser(data))
+//   .then(products => {
+//     let body = { products }
+//     return axios.post(`${baseURL}products/add`, body)
+//   })
+//   .then(res => res)
+//   .catch(err => next(err))
+
+let zip = '81501'
+wholefoods.wholefoodsScrape(zip)
+  .then(data => wholefoods.formatter(data, zip))
   .then(products => {
-    console.log(products)
     let body = { products }
     return axios.post(`${baseURL}products/add`, body)
   })
-  .then(res => res)
-  .catch(err => next(err))
+  .then(res => {
+    console.log('response!',res)
+    return res
+  })
+  .catch(err => {
+    console.log(err)
+    return err
+  })
--- scrapers/wholefoods.js
+++ scrapers/wholefoods.js
@@ -1,16 +1,14 @@
 const puppeteer = require('puppeteer')
 const currentWeekNumber = require('current-week-number')
 
-
 const wholefoodsScrape = async (zip) => {
-  const browser = await puppeteer.launch({ headless: false })
+  const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
   await page.goto(`https://www.wholefoodsmarket.com/stores/list`)
-
   await page.waitForSelector('#edit-field-geo-data-latlon-address')
   await page.focus('#edit-field-geo-data-latlon-address')
   await page.click('#edit-field-geo-data-latlon-address')
-  await page.type('#edit-field-geo-data-latlon-address', `${zip}`,{delay: 100})
+  await page.type('#edit-field-geo-data-latlon-address', `${zip}`)
   await page.keyboard.down('Enter')
   await page.keyboard.up('Enter')
   await page.waitFor(4000)
@@ -24,67 +22,36 @@ const wholefoodsScrape = async (zip) => {
       if (i % 2 === 0) return el
     })
     let finalParsed = await filtered.map(node => {
-      // console.log(node.childNodes[7].childNodes[0].childNodes[0].childNodes[1].innerHTML)
       let imageStr = node.childNodes[7].childNodes[0].childNodes[0].childNodes[1].innerHTML
       let firstHalf
       let secondHalf
-      console.log(node.childNodes[7].childNodes[0].childNodes[0].childNodes[2].childNodes[0])
       if (!node.childNodes[7].childNodes[0].childNodes[0].childNodes[2].childNodes[0] || !node.childNodes[7].childNodes[0].childNodes[0].childNodes[2].childNodes[2]) {
-
-        console.log('dont work')
         return null
       } else {
-        console.log('work')
         firstHalf = node.childNodes[7].childNodes[0].childNodes[0].childNodes[2].childNodes[0].innerText
-        // console.log(node.childNodes[7].childNodes[0].childNodes[0].childNodes[2].childNodes[2].innerText)
         secondHalf = node.childNodes[7].childNodes[0].childNodes[0].childNodes[2].childNodes[2].innerText
       }
-
       let image = imageStr.slice(12, -9)
       let name = node.childNodes[3].innerText.slice(0, -1)
       let price = `Sale ${firstHalf} - ${secondHalf}`
-      return { image, name, price, store: 'Whole Foods', zip}
+      return { image, name, price, store: 'Whole Foods' }
     })
-    console.log(finalParsed)
     return finalParsed
   })
   return finalInfo
-}
-wholefoodsScrape('98177')
-  .then( async (arr) => {
-    let result = await arr.filter(el => {
-      if (el !== null) {
-        el.week = currentWeekNumber()
-        return el
-      }
-    })
-    console.log(result)
-  })
-
-const stringsParser = async (arr) => {
-  // console.log(arr)
-  let firstCut = arr.map(el => el.text.split('src='))
-  // console.log(arr[0]);
-  let onlyImgAndText = []
-  firstCut.forEach(el => {
-    onlyImgAndText.push(el[1])
-  })
-  let dividedImgAndText = []
-  onlyImgAndText.forEach(el => {
-    // console.log(el);
-    let temp = el.split('alt=')
-    dividedImgAndText.push(el.split('alt='))
-  })
-  let finalParsedObjs = []
-  dividedImgAndText.forEach((el, i) => {
-    // console.log(el)
-    let image = el[0].slice(1, el[0].length-2)
-    let name = el[1].slice(1, el[1].length-2).trim()
-    let week = currentWeekNumber()
-    let zip = 'local'
-    finalParsedObjs.push({ image, name, price: arr[i].price, store: 'Target', zip, week })
-  })
-  return finalParsedObjs
+  browser.close()
 }
 
-module.exports={ wholefoodsScrape, stringsParser }
+const formatter = async (arr, zip) => {
+  let result = await arr.filter(el => {
+    if (el !== null) {
+      el.week = currentWeekNumber()
+      el.zip = zip
+      return el
+    }
+  })
+  return result
+}
+
+
+module.exports={ wholefoodsScrape, formatter }
