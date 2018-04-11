61052b1197f88577020a887321e6a4a0b6a24c8f
Justin Hays <jhays124@yahoo.com>
2018-04-10 18:48:28 -0700

    kroger scraper working based off users zip

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 const target = require('./scrapers/target')
 const wholefoods = require('./scrapers/wholefoods')
+const kroger = require('./scrapers/kroger')
 
 //notdeployed
 const baseURL = 'http://localhost:3000/'
@@ -8,14 +9,27 @@ const baseURL = 'http://localhost:3000/'
 //deployed
 //const baseURL ''
 
-target.targetScrape()
-  .then(data => target.stringsParser(data))
+// target.targetScrape()
+//   .then(data => target.stringsParser(data))
+//   .then(products => {
+//     let body = { products }
+//     return axios.post(`${baseURL}products/add`, body)
+//   })
+//   .then(res => res)
+//   .catch(err => next(err))
+
+let zip = '98177'
+kroger.krogerScrape(zip)
   .then(products => {
+    console.log(products.length)
     let body = { products }
     return axios.post(`${baseURL}products/add`, body)
   })
-  .then(res => res)
-  .catch(err => next(err))
+  .then(res => {
+    console.log('response', res)
+    return res
+  })
+  .catch(console.error)
 
 // let zip = '98177'
 // wholefoods.wholefoodsScrape(zip)
--- scrapers/kroger.js
+++ scrapers/kroger.js
@@ -1,34 +1,42 @@
 const puppeteer = require('puppeteer')
+const currentWeekNumber = require('current-week-number')
 
-let targetScrape = async () => {
-  const browser = await puppeteer.launch({ headless: false })
+let krogerScrape = async (zip) => {
+  const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
-  await page.goto(`https://weeklyad.target.com/`)
-  await page.waitFor('body > div.contentWrapper > main > div.ng-scope > div > section > div > div > div > section > div.ad-selector.desktop.ng-isolate-scope.single > div > div > div > div > div.content.col-xs-6 > div:nth-child(1) > div > div > p > button')
-  await page.click('body > div.contentWrapper > main > div.ng-scope > div > section > div > div > div > section > div.ad-selector.desktop.ng-isolate-scope.single > div > div > div > div > div.content.col-xs-6 > div:nth-child(1) > div > div > p > button')
-  await page.waitForSelector('body > div.contentWrapper > main > div.ng-scope > div > div.promotion-banner > span.page-category-btn-container > a.category-view.ng-scope')
-  await page.click('body > div.contentWrapper > main > div.ng-scope > div > div.promotion-banner > span.page-category-btn-container > a.category-view.ng-scope')
-  await page.waitForSelector('body > div.contentWrapper > main > div.ng-scope > div > section > div.category-tiles > ul > li:nth-child(6) > div > div')
-  await page.click('body > div.contentWrapper > main > div.ng-scope > div > section > div.category-tiles > ul > li:nth-child(6) > div > div')
-  await page.waitForSelector('body > div.contentWrapper > main > div.ng-scope > div > section.product-tiles-wrapper.ng-scope > div.product--tiles')
-  await page.waitForSelector('.product--list')
-  let res = await page.evaluate( async () => {
-      let info = await document.querySelector('.product--list')
-      let childArr = Array.from(info.children)
-      let result = await childArr.map((el, i) => {
-        return { i, description: el.innerText }
-      })
-      let big = await document.querySelectorAll('.product--link')
-      big.forEach((el, i) => {
-       result[i]['image'] = el.childNodes[1].childNodes[1].childNodes[1].currentSrc
-      })
-       return result
+  await page.goto(`https://wklyads.fredmeyer.com/flyers/fredmeyer?type=2&locale=en-US&chrome=broadsheet&force_store_selection=true&postal_code=${zip}&filler=#!/flyers/fredmeyer-weekly?flyer_run_id=334937`)
+  await page.waitFor('#postal_code_form > div:nth-child(5) > div > div.submit_postal_code_btn.btn_green')
+  await page.click('#postal_code_form > div:nth-child(5) > div > div.submit_postal_code_btn.btn_green')
+  await page.waitForSelector('#store_select_area > div > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > form > button')
+  await page.click('#store_select_area > div > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > form > button')
+  await page.waitForSelector('#other_flyer_runs > div > div > div > div.other_flyer_runs_wrapper > table > tbody > tr:nth-child(1) > td.flyer-run-details > a > div > p')
+  await page.click('#other_flyer_runs > div > div > div > div.other_flyer_runs_wrapper > table > tbody > tr:nth-child(1) > td.flyer-run-details > a > div > p')
+  await page.waitForSelector('#wishabi-flyerarea > div.wishabi-topbar-container.wishabi-topbar-container-broadsheet > div > div.wishabi-broadsheettopbar-submenu > div.wishabi-broadsheettopbar-submenu-extra > div.wishabi-broadsheettopbar-accessibility-area > div.wishabi-broadsheettopbar-grid-view.goog-inline-block.goog-custom-button')
+  await page.waitFor(1000)
+  await page.click('#wishabi-flyerarea > div.wishabi-topbar-container.wishabi-topbar-container-broadsheet > div > div.wishabi-broadsheettopbar-submenu > div.wishabi-broadsheettopbar-submenu-extra > div.wishabi-broadsheettopbar-accessibility-area > div.wishabi-broadsheettopbar-grid-view.goog-inline-block.goog-custom-button > div > div > div')
+  await page.waitForSelector('#wrapper > div.gridview-wrapper > div > div.items-list > div.category-AllCategories > ul')
+  let infoArray = await page.evaluate( async () => {
+    let info = await document.querySelector('#wrapper > div.gridview-wrapper > div > div.items-list > div.category-AllCategories > ul')
+    let childArr = Array.from(info.children)
+    let result = await childArr.map((el, i) => {
+      let imageStr = el.childNodes[1].childNodes[1].childNodes[1].innerHTML
+      let image = imageStr.slice(19,-16)
+      let name = el.childNodes[7].innerHTML
+      let price = el.childNodes[5].innerText
+      if (price === '') return null
+      return { image, name, price }
     })
-  return res
+    return result
+  })
+  let filtered = infoArray.filter(el => {
+    if (el !== null) {
+      el.zip = zip
+      el.store = 'Kroger'
+      el.week = currentWeekNumber()
+      return el
+    }
+  })
+  return filtered
 }
 
-targetScrape()
-  .then(data => {
-    console.log('done', data)
-    return data
-  })
+module.exports={ krogerScrape }
