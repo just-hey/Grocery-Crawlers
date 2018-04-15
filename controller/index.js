a9124d91b1a0137a62ac4b2c59f3b34ed7fe39cd
Justin Hays <jhays124@yahoo.com>
2018-04-15 14:11:02 -0700

    adding logs

--- controller/index.js
+++ controller/index.js
@@ -5,24 +5,29 @@ class Controller {
   constructor() {}
 
   static scrapeStores(req, res, next) {
+    console.log('scrape stores firing?');
     let { zip } = req.params
     let products = []
     return scrapers.wholefoodsScraper.wholefoodsScrape(zip)
     .then(async (wholeFoodsData) => {
+      console.log('wholeFoods fired');
       await wholeFoodsData.forEach(product => products.push(product))
       return scrapers.targetScraper.targetScrape()
     })
     .then(async (targetStoreData) => {
+      console.log('target fired');
       await targetStoreData.forEach(product => products.push(product))
       return scrapers.krogerScraper.krogerScrape(zip)
     })
     .then(async (krogerStoreData) => {
+      console.log('kroger fired');
       await krogerStoreData.forEach(product => products.push(product))
       let body = { products }
       console.log('final scraped product count === ', body.products.length)
       return axios.post(`${process.env.BASE_URL}products/add`, body)
     })
     .then(response => {
+      console.log(response.data.message);
       return response.data.message
     })
     .catch(console.error)
