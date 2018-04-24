6c9bac47e3be7b605266b6f199d96c4bb387fa46
Justin Hays <jhays124@yahoo.com>
2018-04-24 12:56:55 -0700

    altering logs for scrape process, will still flag success or fail with each run

--- Automated/automatedScrape.js
+++ Automated/automatedScrape.js
@@ -7,45 +7,37 @@ const dbPath = __dirname + '/zipcodes.json'
 const scrapers = require('../scrapers')
 
 const scraperBots = async () => {
-  console.log('scraperBots triggered')
   let zipId
   let zip
   return axios.get(`${baseURL}users/zip`)
     .then(zipReturn => {
       zipId = zipReturn.data.zip.id
       zip = zipReturn.data.zip.zip
-      console.log('return thing is zip? !!!! >>>', zipReturn.data.zip.zip)
       return scrapers.wholefoodsScraper.wholefoodsScrape(zip)
     })
     .then(products => {
-      console.log('wholeFoods scraped',products.length)
       let body = { products }
       return axios.post(`${baseURL}products/add`, body)
     })
     .then(response => {
-      console.log(response.data)
       return scrapers.krogerScraper.krogerScrape(zip)
     })
     .then(products => {
-      console.log('kroger scraped',products.length)
       let body = { products }
       return axios.post(`${baseURL}products/add`, body)
     })
     .then(response => {
-      console.log(response.data)
       return scrapers.targetScraper.targetScrape()
     })
     .then(products => {
-      console.log('target scraped',products.length)
       let body = { products }
       return axios.post(`${baseURL}products/add`, body)
     })
     .then(response => {
-      console.log(response.data)
       return axios.delete(`${baseURL}users/zip/${zipId}`)
     })
     .then(response => {
-      console.log('Im done!', response.data.message)
+      console.log('Scrape done!', response.data.message)
       process.exit(0)
     })
     .catch(error => {
